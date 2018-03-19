48b81f29ea887faba046bed91a569d2b893a0689
Syed Ashiq <syedashiq2005@gmail.com>
2018-03-19 17:19:08 +0400

    Initial commit

---
+++ app/src/main/java/com/develeper/mylearning_11/MainActivity.java
@@ -0,0 +1,17 @@
+package com.develeper.mylearning_11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tomerrosenfeld.customanalogclockview.CustomAnalogClock;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CustomAnalogClock customAnalogClock = (CustomAnalogClock) findViewById(R.id.analog_clock);
+        customAnalogClock.setAutoUpdate(true);
+    }
+}
